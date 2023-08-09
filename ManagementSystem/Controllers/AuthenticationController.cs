ed0cf74aae2545eb818d06e0a7b1349c51492a2e
Lydia_Shu <93233081+shuwenju@users.noreply.github.com>
2023-08-09 18:24:36 -0400

    simplified log-in process

--- ManagementSystem/Controllers/AuthenticationController.cs
+++ ManagementSystem/Controllers/AuthenticationController.cs
@@ -40,7 +40,7 @@ namespace ManagementSystem.Controllers
 
         [HttpPost]
 		[Route("register")]
-		public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+		public async Task<IActionResult> Register([FromBody] RegisterUser registerUser/*, string role*/)
         {
 			// check if user exist, if exist return error
 			var existUser = await _userManager.FindByEmailAsync(registerUser.Email);
@@ -56,12 +56,12 @@ namespace ManagementSystem.Controllers
 				Email = registerUser.Email,
 				SecurityStamp = Guid.NewGuid().ToString(),
 				UserName = registerUser.Username,
-				TwoFactorEnabled = true
+				//TwoFactorEnabled = true
 			};
 
 			//check if role exist
-			if(await _roleManager.RoleExistsAsync(role))
-			{
+			//if(await _roleManager.RoleExistsAsync(role))
+			//{
 				var result = await _userManager.CreateAsync(user, registerUser.Password);
 
 				if (!result.Succeeded)
@@ -71,7 +71,7 @@ namespace ManagementSystem.Controllers
 				}
 
 				//if role exist, asign role to user
-				await _userManager.AddToRoleAsync(user, role);
+				await _userManager.AddToRoleAsync(user, "User");
 
 				// Add token to verify email
 				var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
@@ -81,12 +81,12 @@ namespace ManagementSystem.Controllers
 
 				return StatusCode(StatusCodes.Status200OK,
 					new Response { Status = "Success", Message = "User created successfully, email sent successfully" });
-			}
-			else
-			{
-				return StatusCode(StatusCodes.Status500InternalServerError,
-						new Response { Status = "Error", Message = "Role doesn't exist!" });
-			}
+			//}
+			//else
+			//{
+			//	return StatusCode(StatusCodes.Status500InternalServerError,
+			//			new Response { Status = "Error", Message = "Role doesn't exist!" });
+			//}
 
 
 		}
@@ -116,21 +116,21 @@ namespace ManagementSystem.Controllers
 			var user = await _userManager.FindByNameAsync(loginModel.Username);
 
 			//if user enabled two factor auth, we won't give him the token right away
-			if (user.TwoFactorEnabled)
-			{
-				await _signInManager.SignOutAsync();
-				await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, true);
-				//generate token
-				var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+			//if (user.TwoFactorEnabled)
+			//{
+			//	await _signInManager.SignOutAsync();
+			//	await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, true);
+			//	//generate token
+			//	var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
 
-				var message = new Message(new string[] { user.Email! }, "OTP Confirmation", token);
-				_emailService.SendEmail(message);
+			//	var message = new Message(new string[] { user.Email! }, "OTP Confirmation", token);
+			//	_emailService.SendEmail(message);
 
-				return StatusCode(StatusCodes.Status200OK,
-					new Response { Status = "Success", Message = $"We sent an OTP to your Email {user.Email}" });
-			}
+			//	return StatusCode(StatusCodes.Status200OK,
+			//		new Response { Status = "Success", Message = $"We sent an OTP to your Email {user.Email}" });
+			//}
 
-			if (user != null && await _userManager.CheckPasswordAsync(user,loginModel.Password))
+			if (user != null && await _userManager.CheckPasswordAsync(user,loginModel.Password) && user.EmailConfirmed)
 			{
 				//claimlist creation
 				var authClaims = new List<Claim>
@@ -161,45 +161,45 @@ namespace ManagementSystem.Controllers
 			
 		}
 
-		[HttpPost]
-		[Route("login-2FA")]
-		public async Task<IActionResult> LoginwithOTP(string accesscode, string username)
-		{
-			var user = await _userManager.FindByNameAsync(username);
-			var signIn = await _signInManager.TwoFactorSignInAsync("Email", accesscode, false, false);
-			if (signIn.Succeeded)
-			{
-				if (user != null)
-				{
-					//claimlist creation
-					var authClaims = new List<Claim>
-				{
-					new Claim(ClaimTypes.Name, user.UserName),
-					new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-				};
-					var userRoles = await _userManager.GetRolesAsync(user);
+		//[HttpPost]
+		//[Route("login-2FA")]
+		//public async Task<IActionResult> LoginwithOTP(string accesscode, string username)
+		//{
+		//	var user = await _userManager.FindByNameAsync(username);
+		//	var signIn = await _signInManager.TwoFactorSignInAsync("Email", accesscode, false, false);
+		//	if (signIn.Succeeded)
+		//	{
+		//		if (user != null)
+		//		{
+		//			//claimlist creation
+		//			var authClaims = new List<Claim>
+		//		{
+		//			new Claim(ClaimTypes.Name, user.UserName),
+		//			new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+		//		};
+		//			var userRoles = await _userManager.GetRolesAsync(user);
 
-					foreach (var role in userRoles)
-					{
-						//add roles to the claimlist
-						authClaims.Add(new Claim(ClaimTypes.Role, role));
-					}
+		//			foreach (var role in userRoles)
+		//			{
+		//				//add roles to the claimlist
+		//				authClaims.Add(new Claim(ClaimTypes.Role, role));
+		//			}
 
 
-					//generate token with cliams
-					var jwtToken = GetToken(authClaims);
+		//			//generate token with cliams
+		//			var jwtToken = GetToken(authClaims);
 
-					return Ok(new
-					{
-						token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
-						expiration = jwtToken.ValidTo
-					});
-					//return the token
-				}
-			}
-			return StatusCode(StatusCodes.Status404NotFound,
-				new Response { Status = "Not Found", Message = "Invalid Code" });
-		}
+		//			return Ok(new
+		//			{
+		//				token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+		//				expiration = jwtToken.ValidTo
+		//			});
+		//			//return the token
+		//		}
+		//	}
+		//	return StatusCode(StatusCodes.Status404NotFound,
+		//		new Response { Status = "Not Found", Message = "Invalid Code" });
+		//}
 
 		[HttpPost]
 		[Route("forgot-password")]
