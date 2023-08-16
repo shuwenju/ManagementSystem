455878c38fc5ce3a708b16e637a203a947d45930
Manny <krav23@hotmail.com>
2023-08-16 13:28:24 -0400

    Edit Styling

--- newFrontend/src/Routes/AdminRoutes.js
+++ newFrontend/src/Routes/AdminRoutes.js
@@ -1,14 +1,8 @@
 import React, { Fragment } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Dashboard } from "../pages/Dashboard";
-import { Customers } from "../pages/Customers";
-import { Products } from "../pages/Products";
-import { Orders } from "../pages/Orders";
+
 import { OrderDetails } from "../pages/OrderDetails";
 import { AddOrder } from "../pages/AddOrder";
-import { Reports } from "../pages/Reports";
-import { Setting } from "../pages/Setting";
-import { Employees } from "../pages/Employees";
+
 import { useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
@@ -32,6 +26,7 @@ const AdminRoutes = () => {
         <Route path="customers" element={<Customers />} />
         <Route path="products" element={<Products />} />
         <Route path="orders" element={<Orders />} />
+        <Route path="add-order" element={<AddOrder />} />
         <Route path="reports" element={<Reports />} />
         <Route path="setting" element={<Setting />} />
       </Route>
