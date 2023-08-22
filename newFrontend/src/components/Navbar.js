b5ba204d35a4ecd6ab12ca6d82a9e89a42d187da
mrabi24 <120145506+mrabi24@users.noreply.github.com>
2023-08-22 10:55:57 -0400

    login and navbar
    added links in the tracking page back to login, added functions in the login to get the full name of user and display it in navbar

--- newFrontend/src/components/Navbar.js
+++ newFrontend/src/components/Navbar.js
@@ -1,32 +1,54 @@
-import {CDBIcon} from "cdbreact";
-import {NavLink} from "react-router-dom";
+import { CDBIcon } from "cdbreact";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import RoleContext from "../data/RoleContext";
 
-const Navbar = ({onToggleSideBar}) => {
+const Navbar = ({ onToggleSideBar }) => {
+  const { role } = useContext(RoleContext);
+  const userFullName = localStorage.getItem("userFullName");
 
-    return (
-        <nav className="navbar navbar-expand-md text-light" style={{background: "#c84042"}}>
-            <div className="container">
-                <NavLink className="navbar-brand" to="/">
-                    <img alt="logoImage" src="/img/goat-head.png" style={{width: "3.5rem", height: "3.5rem"}}/>
-                </NavLink>
-                <CDBIcon fas icon="bars" size="lg" style={{cursor: 'pointer', marginLeft: '5rem'}}
-                         onClick={onToggleSideBar}/>
-                <ul className="navbar-nav ms-auto align-items-center">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/setting">
-                            <img alt="panelImage" src="/img/profile.png" style={{width: "3rem", height: "3rem"}}/>
-                            <span className="text-light ms-1">Matthew Parker</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-light" href="/">
-                            <CDBIcon fas icon="power-off" size="lg"/>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav
+      className="navbar navbar-expand-md text-light"
+      style={{ background: "#c84042" }}
+    >
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          <img
+            alt="logoImage"
+            src="/img/goat-head.png"
+            style={{ width: "3.5rem", height: "3.5rem" }}
+          />
+        </NavLink>
+        <CDBIcon
+          fas
+          icon="bars"
+          size="lg"
+          style={{ cursor: "pointer", marginLeft: "5rem" }}
+          onClick={onToggleSideBar}
+        />
+        <ul className="navbar-nav ms-auto align-items-center">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/setting">
+              <img
+                alt="panelImage"
+                src="/img/profile.png"
+                style={{ width: "3rem", height: "3rem" }}
+              />
+              <span className="text-light ms-1">
+                {role === "Admin" ? "Admin" : userFullName}
+              </span>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-light" href="/">
+              <CDBIcon fas icon="power-off" size="lg" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
