41937f7fd0a81ec39ca4da6178740faa2c9079b5
ramezbetrus <ramez.betrus@gmail.com>
2023-08-22 13:03:07 -0400

    add ProfileController

---
+++ ManagementSystem/Models/Authentication/Signup/ChangePassword.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagementSystem.Models.Authentication.Signup
+{
+    public class ChangePassword
+    {
+        [Required]
+        public string CurrentPassword { get; set; } = string.Empty;
+
+        [Required]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
